0eb19f2fb5d081f902889911ce0e325c656477bd
varun <varunlad5@gmail.com>
2021-09-06 18:17:21 +0530

    [varun] User Case 1 start of snake and ladder code

---
+++ SnakeandLadder058/UC1snakeandladder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadder058
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int player1 = 0;
+            while (player1 <= 100)
+            {
+                Console.WriteLine(" position of player 1 is : " + player1);
+                player1++;
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
